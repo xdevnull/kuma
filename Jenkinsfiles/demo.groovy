e8ef5c08ff65ecf53e98e89a0d911412b07fe140
Josh Mize <jgmize@gmail.com>
2017-02-03 15:51:37 -0600

    Switch from k8s migrate to deis migrate

--- Jenkinsfiles/demo.groovy
+++ Jenkinsfiles/demo.groovy
@@ -16,6 +16,6 @@ stage('Deploy') {
   // make: *** [deis-config] Error 1
   sh "KUBECONFIG=${env.KUBECONFIG} kubectl --namespace=${env.DEIS_APP} apply -f k8s/"
   sh 'make deis-pull'
-  sh 'make k8s-migrate'
+  sh 'make deis-migrate'
   sh 'make deis-scale-worker'
 }
